b8bf491bc2a9f8d394be76959071c17535155728
DESKTOP-K7FN4SI\hi <choemyeonggeon@gmail.com>
2023-10-05 19:08:08 +0900

    feature/myeong

--- src/main/java/com/allin/filmface/choiceContents/youtube/entity/Youtube.java
+++ src/main/java/com/allin/filmface/choiceContents/youtube/entity/Youtube.java
@@ -1,27 +1,38 @@
-package com.allin.filmface.choiceContents.youtube.entity;
-
-import lombok.*;
-
-import javax.persistence.*;
-
-@Entity
-@Table(name = "youtube")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class Youtube {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int youtubeNo;
-
-    @Column(nullable = false)
-    private String youtubeLink;
-
-    @Column(nullable = false)
-    private int youtubeCategory;
-
-    @Column(nullable = false)
-    private int youtubeViews;
-}
+//package com.allin.filmface.choiceContents.youtube.entity;
+//
+//import com.allin.filmface.choiceContents.youtube.dto.YoutubeDTO;
+//import com.fasterxml.jackson.annotation.JsonBackReference;
+//import lombok.*;
+//import org.hibernate.annotations.Comment;
+//
+//import javax.persistence.*;
+//
+//@Entity(name = "Youtube")
+//@Table(name = "youtube")
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class Youtube {
+//
+//    @Id
+//    @Column(name = "YOUTUBE_NO")
+//    private int youtubeNo;
+//
+//    @Column(name = "YOUTUBE_LINK")
+//    private String youtubeLink;
+//
+//    @Column(name = "YOUTUBE_VIEWS")
+//    private int youtubeViews;
+//
+//
+//    @Column(name = "YOUTUBE_TITLE")
+//    private String youtubeTitle;
+//
+////        @ManyToOne
+////    @JoinColumn(name = "EMOTION_NO", referencedColumnName = "EMOTION_NO")
+////    @JsonBackReference
+////    private Emotion emotion;
+//
+//
+//}
