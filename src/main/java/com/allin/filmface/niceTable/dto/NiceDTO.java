f82dca836ff17fc86639e91f54da84480a10e729
DESKTOP-4CGQ09I\hi <ljsung6@naver.com>
2023-10-12 17:52:20 +0900

    no message

---
+++ src/main/java/com/allin/filmface/niceTable/dto/NiceDTO.java
@@ -0,0 +1,19 @@
+//package com.allin.filmface.niceTable.dto;
+//
+//import com.allin.filmface.search.dto.SearchDTO;
+//import lombok.*;
+//
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Setter
+//@Getter
+//@ToString
+//public class NiceDTO {
+//
+//    private SearchDTO search;
+//
+//    private int niceNo;
+//    private int niceCount;
+//
+//
+//}
---
+++ src/main/java/com/allin/filmface/niceTable/repository/NiceRepository.java
@@ -0,0 +1,12 @@
+//package com.allin.filmface.niceTable.repository;
+//
+//import com.allin.filmface.niceTable.entity.Nice;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.Optional;
+//
+//@Repository
+//public interface NiceRepository extends JpaRepository<Nice, Long> {
+//    Optional<Nice> findBySearch_SearchNo(int searchNo);
+//}
