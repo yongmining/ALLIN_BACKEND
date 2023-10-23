247cb8b8836ba5cb676557104ba4535db7d41b14
SHIN.sped <sml2031@gmail.com>
2023-10-23 14:47:47 +0900

    1023

--- src/main/java/com/allin/filmface/emotion/controller/PictureController.java
+++ src/main/java/com/allin/filmface/emotion/controller/PictureController.java
@@ -1,34 +1,43 @@
-//package com.allin.filmface.emotion.controller;
-//
-//import com.allin.filmface.emotion.dto.EmotionDTO;
 //import com.allin.filmface.emotion.entity.Picture;
-//import com.allin.filmface.picture.entity.service.PictureService;
-//import lombok.RequiredArgsConstructor;
+//import com.allin.filmface.emotion.service.PictureService;
+////import com.allin.filmface.member.entity.Member;
+//import io.swagger.annotations.Api;
+//import lombok.AllArgsConstructor;
 //import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.multipart.MultipartFile;
 //import org.springframework.web.server.ResponseStatusException;
 //import org.springframework.http.HttpStatus;
-//import java.util.NoSuchElementException;
 //
+//import java.util.NoSuchElementException;
+//import java.security.Principal; // Principal import 추가
+//
+//@Api(tags = "감정분석 관련 API")
 //@RestController
-//@RequiredArgsConstructor
+//@RequestMapping("/api/v1")
+//@AllArgsConstructor
 //public class PictureController {
 //
 //    private final PictureService pictureService;
 //
-//    @GetMapping("/api/v1/pictures/{id}")
-//    public ResponseEntity<Picture> getPicture(RequestBody EmotionDTO emotionDTO) {
+//    @GetMapping("/pictures/{id}")
+//    public ResponseEntity<Picture> getPicture(@PathVariable Long id) {
 //        try {
-//            // PictureService 클래스의 findById() 메서드를 호출하여 이미지를 조회합니다.
 //            Picture picture = pictureService.findById(id);
-//
-//            // 조회된 이미지를 ResponseEntity 객체에 담아서 반환합니다.
 //            return ResponseEntity.ok(picture);
 //        } catch (NoSuchElementException e) {
-//            // 이미지가 존재하지 않을 경우 예외 처리
 //            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Picture not found.");
+//        }
+//    }
+//
+//    @PostMapping("/upload")
+//    public ResponseEntity<String> uploadPicture(@RequestParam("image") MultipartFile file, Principal principal) {
+//        try {
+//            String username = principal.getName(); // 현재 로그인한 사용자 이름 (username)을 얻음
+//            Picture savedPicture = pictureService.save(file, username);
+//            return ResponseEntity.ok("File uploaded successfully. Picture ID: " + savedPicture.getPictureNo());
+//        } catch (Exception e) {
+//            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to upload the file.");
 //        }
 //    }
 //}
--- src/main/java/com/allin/filmface/emotion/dto/PictureDTO.java
+++ src/main/java/com/allin/filmface/emotion/dto/PictureDTO.java
@@ -1,13 +1,20 @@
-//package com.allin.filmface.emotion.dto;
-//
-//import lombok.*;
-//
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Setter
-//@Getter
-//@ToString
-//public class PictureDTO {
-//
-//    private int pictureNo;
-//}
+package com.allin.filmface.emotion.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+public class PictureDTO {
+    private int pictureNo;
+
+    public int getPictureNo() {
+        return pictureNo;
+    }
+
+    public void setPictureNo(int pictureNo) {
+        this.pictureNo = pictureNo;
+    }
+}
